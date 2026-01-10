cdfd0c4298b3d9d4eabca7696600e4d43fe2de17
Gzyms69 <dawidekczerwinski@gmail.com>
2026-01-11 00:35:21 +0100

    Fix Build: Remove webpack alias for 'three' since npm dedupes it automatically, restoring access to 'three/webgpu' exports

--- website/next.config.js
+++ website/next.config.js
@@ -1,19 +1,10 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
-
 const nextConfig = {
   output: 'export',
   basePath: '/WikiGraph',
   assetPrefix: '/WikiGraph/',
   images: {
     unoptimized: true,
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'three': path.resolve(__dirname, 'node_modules/three'),
-    };
-    return config;
   },
 };
 
